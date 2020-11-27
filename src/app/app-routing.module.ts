4cb0d0748f9864a51ed3516b1d03ca0ab4a3e5b1
muzhangxion <254497987@qq.com>
2020-11-27 23:29:21 +0800

    测试模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'test',
+    pathMatch: 'full'
+  },
+  {
+    path: 'test',
+    component: TestComponent,
+    data: {
+      title: '测试'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
