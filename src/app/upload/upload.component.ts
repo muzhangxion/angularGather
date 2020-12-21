0a5b012a33a4e95eb961684edd68883e2494a0b3
muzhangxion <254497987@qq.com>
2020-12-21 20:22:48 +0800

    文件上传

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  uploadArr: Array<any> = []; // 文件数组
 
   constructor() { }
 
@@ -19,8 +20,47 @@ export class UploadComponent implements OnInit {
     }, 1000)
   }
 
-  onUpload() {
-    console.log(1)
+  getUpload(evt: any) {
+    let isImageFlag = true;
+    const target = evt.target;
+
+    const file = target.files[0];
+    const myReader = new FileReader();
+    const that = this;
+    const size = file.size / (1024 * 1024);
+    const reg = /jpg|jpeg|png|gif|doc|docx|xls|xlsx|pdf|ceb|text|txt|wps|et/i;
+    const targetName = file.name;
+    const indeX = targetName.indexOf('.');
+    const uploadFileName = targetName.substr(0, indeX);
+    if (size > 2) {
+        // that.message.warning('请上传低于2M的文件');
+        target.value = '';
+        return;
+    }
+    if (!reg.test(targetName)) {
+        // that.message.warning('不支持该文件格式');
+        target.value = '';
+        return;
+    }
+    if (/jpg|jpeg|png|gif/i.test(targetName)) {
+        isImageFlag = true;
+    } else {
+        isImageFlag = false
+    }
+    myReader.onloadend = function (loadEvent: any) {
+        const fileArray = {
+            src: loadEvent.target.result,
+            fileName: targetName,
+            uploadFileName: uploadFileName,
+            isImageFlag: isImageFlag,
+            file: file,
+            size: file.size
+        };
+        that.uploadArr.push(fileArray)
+        console.log(that.uploadArr)
+        target.value = '';
+    };
+    myReader.readAsDataURL(file);
   }
 
 }
